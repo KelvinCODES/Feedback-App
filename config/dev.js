1879a5a64cf7e6625431f88c71b9745c222349f6
Kelvin Kong <kkong@enersource.com>
2017-08-10 18:06:33 -0400

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+// dev.js - don't commit this!!!
+module.exports = {
+  googleClientID:
+    '180131079330-qde53t3gvmfuelfk3u71ujg85nkd3nlr.apps.googleusercontent.com',
+  googleClientSecret: 'RpffjWiEckLb8RlryD8x837t',
+  mongoURI:
+    'mongodb://kelvin:Kelvinkong30@ds143892.mlab.com:43892/feedback-dev',
+  cookieKey: 'jdsnfkjdsnfkjdsnfkjsnfdksndkjflnsa'
+};
